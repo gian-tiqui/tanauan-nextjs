0c6bb35c1a92ef9a9232bd5286ae344190282544
gian-tiqui <michaelgiantiqui3@gmail.com>
2024-02-15 10:49:17 +0800

    skdnakndkass

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
+import axios from "axios";
 import React from "react";
 
 const Home = () => {
+  const test = async () => {
+    try {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+
+      if (res.status === 200) {
+        console.log(res.data);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  test();
+
   return <div>hi</div>;
 };
 
